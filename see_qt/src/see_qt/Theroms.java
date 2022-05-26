99fbc93e01c531abaf2d7a36d188706ec91c37ec
admin <admin@Bcalab3-m14>
2022-05-26 10:20:38 +0530

    commit (push)

---
+++ see_qt/src/see_qt/Theroms.java
@@ -0,0 +1,27 @@
+package see_qt;
+
+public class Theroms {
+	
+	int volume;
+	String brand;
+	String liquid;
+	int capacity = 0;
+	
+	public Theroms(int volume, String brand)
+	{
+		this.volume=volume;
+		this.brand=brand;
+	}
+	
+	public void addliquid(String liquid)
+	{
+		this.liquid=liquid;
+		capacity=1;
+	}
+	
+	public void display() {
+		System.out.println("Volume" + volume);
+		System.out.println(" Brand" + brand);
+		System.out.println("Liquid"+liquid);
+	}
+}
